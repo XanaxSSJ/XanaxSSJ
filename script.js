76ddbbd9db40f80c2352843f5f5c96f7cc604c35
XanaxSSJ <sebastiangiron25@gmail.com>
2025-08-15 05:52:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let text = "XanaxSSJ";
+let index = 0;
+let direction = 1;
+
+function animateTitle() {
+    document.title = text.slice(0, index);
+
+    if (direction === 1) {
+        index++;
+        if (index > text.length) {
+            direction = -1;
+            index = text.length;
+        }
+    } else {
+        index--;
+        if (index < 0) {
+            direction = 1;
+            index = 0;
+        }
+    }
+
+    setTimeout(animateTitle, 350);
+}
+
+fetch('https://api.countapi.xyz/hit/miusuario.github.io/contador')
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('visitas').textContent = data.value;
+    });
